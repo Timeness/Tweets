0df306f8126767030a2362e0488bb68382b25191
Timeness <careless@flash.co>
2025-01-20 23:33:57 +0530

    done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const { Bot } = require("grammy");
+const fetch = require("node-fetch");
 
 const BOT_TOKEN = "8040058187:AAEykWGMhrv9lWlFrj-W9rwPiPENI2ZuHXw";
 const CHANNEL_ID = "-1002333484577";
@@ -42,11 +43,43 @@ async function postToTelegram(message) {
 
 setInterval(fetchLatestTweet, 60000);
 
+bot.command("start", (ctx) => ctx.reply("Heya; I will post leatest tweets here use /fetch {username} to get the latest tweets."));
 
-bot.command("start", (ctx) => ctx.reply("Heya; I will post leatest tweets here."));
+async function fetchLatestTweetByUsername(username) {
+    const response = await fetch(`https://api.twitter.com/2/users/by/username/${username}/tweets`, {
+        headers: {
+            Authorization: `Bearer AAAAAAAAAAAAAAAAAAAAAFZpyQEAAAAAp7k5DqPP65jx29YamyqM0NgsV%2FM%3D41Zh40EF5F7PQwU5iCZiybISIGntiuINEpW8BV0GP2cwwoqJh0`,
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`Error fetching tweets: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
 bot.command("fetch", async (ctx) => {
-    await fetchLatestTweet();
-    ctx.reply("Fetched latest tweets !");
+    const args = ctx.message.text.split(" ");
+    if (args.length < 2) {
+        await fetchLatestTweet();
+        ctx.reply("Fetched latest tweets !");
+        return ctx.reply("Please provide a Twitter username. Example: /fetch TwitterHandle");
+    }
+
+    const username = args[1];
+
+    try {
+        const tweets = await fetchLatestTweet(username);
+        if (tweets.data && tweets.data.length > 0) {
+            ctx.reply(`Latest tweet from @${username}: ${tweets.data[0].text}`);
+        } else {
+            ctx.reply(`No tweets found for @${username}.`);
+        }
+    } catch (error) {
+        ctx.reply(`Error: ${error.message}`);
+    }
 });
 
 bot.start();
